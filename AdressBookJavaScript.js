f215bd3be0d9f34b96652bac711350effc4a8f8e
Bathala-Haresh <hareshbathala87@gmail.com>
2021-08-24 21:38:53 +0530

    Added:program to dd contacts

--- AdressBookJavaScript.js
+++ AdressBookJavaScript.js
@@ -1,52 +1,159 @@
-//UC-1
 class Contact {
   // Constructor for the different fields of contact
   constructor(...params) {
-      this.firstName = params[0];
-      this.lastName = params[1];
-      this.address = params[2];
-      this.city = params[3];
-      this.state = params[4];
-      this.zip = params[5];
-      this.phoneNumber = params[6];
-      this.email = params[7];
+    this.firstName = params[0];
+    this.lastName = params[1];
+    this.address = params[2];
+    this.city = params[3];
+    this.state = params[4];
+    this.zip = params[5];
+    this.phoneNumber = params[6];
+    this.email = params[7];
   }
-// Getter & Setter method
-get firstName() { return this._firstName; }
-set firstName(firstName) {
-  this._firstName = firstName;
+  // Getter & Setter method
+  get firstName() {
+    return this._firstName;
+  }
+  set firstName(firstName) {
+    let firstNameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (firstNameRegex.test(firstName)) this._firstName = firstName;
+    else throw "Invalid First name";
+  }
+  get lastName() {
+    return this._lastName;
+  }
+  set lastName(lastName) {
+    let lastNameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (lastNameRegex.test(lastName)) this._lastName = lastName;
+    else throw "Invalid last Name";
+  }
+  get address() {
+    return this._address;
+  }
+  set address(address) {
+    let addressRegex = RegExp("^[A-Za-z]{4,}$");
+    if (addressRegex.test(address)) this._address = address;
+    else throw "Invalid Address";
+  }
+  get city() {
+    return this._city;
+  }
+  set city(city) {
+    let cityRegex = RegExp("^[A-Za-z]{4,}$");
+    if (cityRegex.test(city)) this._city = city;
+    else throw "Invalid City name";
+  }
+  get state() {
+    return this._state;
+  }
+  set state(state) {
+    let stateRegex = RegExp("^[A-Za-z]{4,}$");
+    if (stateRegex.test(state)) this._state = state;
+    else throw "Invalid State name";
+  }
+  get zip() {
+    return this._zip;
+  }
+  set zip(zip) {
+    let zipRegex = RegExp("^[0-9]{3}[ ]?[0-9]{3}$");
+    if (zipRegex.test(zip)) this._zip = this.zip;
+    else throw "Invalid Zipcode";
+  }
+  get phoneNumber() {
+    return this._phoneNumber;
+  }
+  set phoneNumber(phoneNumber) {
+    let phoneNumberRegex = RegExp("^([0-9]{2}[ ])?[6-9]{1}[0-9]{9}$");
+    if (phoneNumberRegex.test(phoneNumber)) this._phoneNumber = phoneNumber;
+    else throw "Invalid Phone Number";
+  }
+  get email() {
+    return this._email;
+  }
+  set email(email) {
+    let emailRegex = RegExp(
+      "^[a-z]{1}[a-z0-9]{2,}([._+-]?[a-z0-9]+)?[@]{1}[a-z1-9]+[.]{1}(co|net|com|gov)([.]{1}[a-z]{2,3})?$"
+    );
+    if (emailRegex.test(email)) this._email = email;
+    else throw "Invalid Email";
+  }
+  //To string method for displaying contacts
+  toString() {
+    return (
+      "First Name: " +
+      this.firstName +
+      " Last Name: " +
+      this.lastName +
+      " Address: " +
+      this.address +
+      " City: " +
+      this.city +
+      " State: " +
+      this.state +
+      " Zipcode: " +
+      this.zip +
+      " Phone Number: " +
+      this.phoneNumber +
+      " Email: " +
+      this.email
+    );
+  }
 }
-get lastName() { return this._lastName; }
-set lastName(lastName) {
-  this._lastName = lastName;
+let AddressBook = new Array();
+function AddContact(
+  firstName,
+  lastName,
+  address,
+  city,
+  state,
+  zip,
+  phoneNumber,
+  email
+) {
+  let newcontact = new Contact(
+    firstName,
+    lastName,
+    address,
+    city,
+    state,
+    zip,
+    phoneNumber,
+    email
+  );
+  AddressBook.push(newcontact);
 }
-get address() { return this._address; }
-set address(address) {
-  this._address = address;
+function Main() {
+  console.log("Welcome to address book");
+  AddContact(
+    "Prudhvi",
+    "Reddy",
+    "Tirupati",
+    "Chittoor",
+    "AndhraPradesh",
+    "517 502",
+    "91 7654321034",
+    "prudhvi@gmail.com"
+  );
+  AddContact(
+    "Joe",
+    "Biden",
+    "RedFort",
+    "Delhi",
+    "Delhi",
+    "432 765",
+    "91 7654321098",
+    "bieden@gmail.com"
+  );
+  AddContact(
+    "Sukhla",
+    "Singh",
+    "GhatRoad",
+    "Kolkatta",
+    "Kolkatta",
+    "517 644",
+    "91 8976532214",
+    "sukhla@gmail.com"
+  );
 }
-get city() { return this._city; }
-set city(city) {
-  this._city = city;
-}
-get state() { return this._state; }
-set state(state) {
-  this._state = state;
-}
-get zip() { return this._zip; }
-set zip(zip) {
-  this._zip = this.zip;
-}
-get phoneNumber() { return this._phoneNumber; }
-set phoneNumber(phoneNumber) {
-  this._phoneNumber = phoneNumber;
-}
-get email() { return this._email; }
-set email(email) {
-  this._email = email;
-}
-//To string method for displaying contacts
-toString() {
-  return "First Name: " + this.firstName + " Last Name: " + this.lastName + " Address: " + this.address + " City: " + this.city
-      + " State: " + this.state + " Zipcode: " + this.zip + " Phone Number: " + this.phoneNumber + " Email: " + this.email;
-}
-}
+Main();
+AddressBook.forEach((contact) => console.log(contact.toString()));
